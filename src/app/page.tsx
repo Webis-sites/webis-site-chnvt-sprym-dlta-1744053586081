3b2169b899a0a6748c492f36c10d18c59cabf0af
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-07 22:57:08 +0300

    Add LocationSection component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import AboutSection from '../components/AboutSection';
 import ServicesSection from '../components/ServicesSection';
 import TestimonialsSection from '../components/TestimonialsSection';
 import ContactSection from '../components/ContactSection';
+import LocationSection from '../components/LocationSection';
 
 export default function HomePage() {
   return (
@@ -19,6 +20,7 @@ export default function HomePage() {
     <ServicesSection />
     <TestimonialsSection />
     <ContactSection />
+    <LocationSection />
   </main>
       
       <footer className="py-6 bg-gray-100 dark:bg-gray-800">
